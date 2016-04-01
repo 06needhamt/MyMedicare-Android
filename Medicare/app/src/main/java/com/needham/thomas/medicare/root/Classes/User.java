9799e7d8b99175070c6d5032cd45d7e1631e3e79
Tom Needham <06needhamt@gmail.com>
2016-04-01 12:11:15 +0100

    Organise classes into packages

---
+++ Medicare/app/src/main/java/com/needham/thomas/medicare/root/Classes/User.java
@@ -0,0 +1,8 @@
+package com.needham.thomas.medicare.root.Classes;
+
+/**
+ * Created by Thomas Needham on 01/04/2016.
+ * This class represents an end user within the app
+ */
+public class User {
+}
