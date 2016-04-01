6f19c13d4aa422efda14e4b058b28ce7d8315017
Tom Needham <06needhamt@gmail.com>
2016-04-01 10:33:57 +0100

    Create Welcome Screen

--- Medicare/app/src/main/java/com/needham/thomas/medicare/MainActivity.java
+++ Medicare/app/src/main/java/com/needham/thomas/medicare/MainActivity.java
@@ -1,13 +1,86 @@
 package com.needham.thomas.medicare;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    int width;
+    int height;
+    DisplayMetrics dm;
+    TextView title;
+    Button btnRegister;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dm = new DisplayMetrics();
+        GetDisplayMetrics(dm);
+        SetupViews();
+
+    }
+
+    /**
+     * This function sets up the user interface
+     */
+    private void SetupViews() {
+        title = (TextView) findViewById(R.id.welcome);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        SetupTitleLayout();
+        SetupLoginButtonLayout();
+        SetupRegisterButtonLayout();
+    }
+
+    private void SetupRegisterButtonLayout() {
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width,height);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.width = FrameLayout.LayoutParams.MATCH_PARENT;
+        params.height = FrameLayout.LayoutParams.WRAP_CONTENT;
+        params.topMargin = (int) (height * 0.50);
+        btnRegister.setGravity(Gravity.CENTER);
+        btnRegister.setLayoutParams(params);
+    }
+
+    private void SetupLoginButtonLayout() {
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width,height);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.width = FrameLayout.LayoutParams.MATCH_PARENT;
+        params.height = FrameLayout.LayoutParams.WRAP_CONTENT;
+        params.topMargin = (int) (height * 0.40);
+        btnLogin.setGravity(Gravity.CENTER);
+        btnLogin.setLayoutParams(params);
+    }
+
+    /**
+     * This function sets up the layout for the title text view
+     */
+    private void SetupTitleLayout() {
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width,height);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.width = FrameLayout.LayoutParams.MATCH_PARENT;
+        params.height = FrameLayout.LayoutParams.WRAP_CONTENT;
+        params.topMargin = (int) (height * 0.01);
+        title.setGravity(Gravity.CENTER);
+        title.setLayoutParams(params);
+    }
+
+    /**
+     * gets the screen width and size for dynamic creation of the ui
+     * @param dm the display metrics object to store the values in
+     */
+    protected void GetDisplayMetrics(DisplayMetrics dm) {
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        width = dm.widthPixels;
+        height = dm.heightPixels;
     }
 }
