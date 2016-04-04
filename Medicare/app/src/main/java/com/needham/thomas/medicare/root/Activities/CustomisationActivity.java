6b134c6f463c7b96d44d84d9c410721a2cca789f
Tom Needham <06needhamt@gmail.com>
2016-04-04 11:30:56 +0100

    Create customisation Activity

---
+++ Medicare/app/src/main/java/com/needham/thomas/medicare/root/Activities/CustomisationActivity.java
@@ -0,0 +1,15 @@
+package com.needham.thomas.medicare.root.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.needham.thomas.medicare.R;
+
+public class CustomisationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customisation);
+    }
+}
